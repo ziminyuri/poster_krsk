f6b55569b89e90c88043a1b7dea473cd3e49dd39
ziminyuri <zimin.yuri22@gmail.com>
2020-04-05 20:03:23 +0700

    :card_file_box:Добавил модели Профиль пользователя, Тип события, событие в класс моделей

--- poster_app/models.py
+++ poster_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserProfile(models.Model):
+    ID_user_profile = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    surname = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=17)
+
+    class Meta:
+        db_table = 'user_profile'
+
+
+class TypeEvent(models.Model):
+    ID_type_event = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'type_event'
+
+
+class Event(models.Model):
+    ID_event = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    img_path = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    data_time = models.DateTimeField()
+    ticket_price = models.CharField(max_length=15)
+    number_of_tickets = models.CharField(max_length=7)
+    ID_type_event = models.ForeignKey(TypeEvent, models.DO_NOTHING)
+    ID_user_profile = models.ForeignKey(UserProfile, models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'event'
+
+
--- poster_app/views.py
+++ poster_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
 
+
 def index(request):
     return render(request, 'poster_app/index.html')
